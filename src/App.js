1a7f7251a272fb06924f090e0c93be615fa99269
mike mendes <4mikemendes@gmail.com>
2021-10-29 11:07:09 +0200

    setup2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+render(){
   return (
     <div className="App">
-      
+      <h1 className= "title">React Movie App</h1>
     </div>
-  );
+  )
+}
 }
 
 export default App;
