ca6a16a23bf800b1b4042de87ca5c351aff2d4c0
mike mendes <4mikemendes@gmail.com>
2021-11-01 14:42:46 +0100

    roseta

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -7,7 +7,19 @@ function SearchMovies() {
     const searchMovies = async (e) => {
         e.preventDefault()
         console.log("searching")
+        try {
+            const query = "some movie"
+            
+            const url = `https://api.themoviedb.org/3/movie/550?api_key=8b8f7a79ae2f460a57596eb85e1b8701&language=en-us&query=${query}&page=1&include_adult=false`;
+            
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data)
+        }catch (err) {
+            console.log(err)
+        }
     }
+
 
     return(
     <form className="form" onSubmit={searchMovies}>
