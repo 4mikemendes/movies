f585080c01baf1854615acb3483b8629c0b38f5f
mike mendes <4mikemendes@gmail.com>
2021-11-01 06:56:43 +0100

    style search comp

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "../styles/SearchMovies.css"
 
+
 function SearchMovies() {
+
+    const searchMovies = async (e) => {
+        e.preventDefault()
+        console.log("searching")
+    }
+
     return(
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
         <label htmlFor="query" className="lable">Movie Name</label>
         <input className="input" type="text" name="query" placeholder="i.e. Inception"/>
         <button className=" button" type="submit">Submit</button>
