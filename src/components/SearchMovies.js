e4634e70520f7912749accd5cd4b89286a4b595b
mike mendes <4mikemendes@gmail.com>
2021-11-02 08:03:42 +0100

    url

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState} from "react"
 import "../styles/SearchMovies.css"
 
 
 function SearchMovies() {
 
+    const [query, setQuery] = useState(" ")
+
     const searchMovies = async (e) => {
         e.preventDefault()
-        console.log("searching")
+        
+        
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=8b8f7a79ae2f460a57596eb85e1b8701&language=en-us&query=${query}&page=1&include_adult=false`;
+
         try {
-            const query = "some movie"
-            
-            const url = `https://api.themoviedb.org/3/movie/550?api_key=8b8f7a79ae2f460a57596eb85e1b8701&language=en-us&query=${query}&page=1&include_adult=false`;
-            
             const res = await fetch(url)
             const data = await res.json()
             console.log(data)
@@ -24,7 +25,9 @@ function SearchMovies() {
     return(
     <form className="form" onSubmit={searchMovies}>
         <label htmlFor="query" className="lable">Movie Name</label>
-        <input className="input" type="text" name="query" placeholder="i.e. Inception"/>
+        <input className="input" type="text" name="query" 
+        placeholder="i.e. Inception" value={query} 
+        onChange={(e) => setQuery(e.target.value)} />
         <button className=" button" type="submit">Submit</button>
     </form>
     )
